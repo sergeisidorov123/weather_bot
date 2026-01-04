c81304623bb57924cb0a54a5cd4fe4c6b6fc32b5
Sergei <seregasidorov2015@mail.ru>
2026-01-04 21:19:21 +0500

    Empty str for token

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    BOT_TOKEN: str
+    BOT_TOKEN: str = " "
     DATABASE_URL: str = "sqlite:///./weatherbot.db"
 
     class Config:
